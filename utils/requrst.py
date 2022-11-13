5ee4e5805d4ae7844202450f2080280a72e076df
RainbowPuppeteer <chengzong.zhao@gmail.com>
2022-11-13 16:47:03 -0500

    headers

--- utils/requrst.py
+++ utils/requrst.py
@@ -4,7 +4,13 @@ import re
 import urllib
 from urllib.parse import quote
 
+
 def dragyoutube(link):
+    headers = {
+        'origin': 'www.youtube.com',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+
     query_string = urllib.parse.urlencode({"search_query": link})
     html_content = urllib.request.urlopen("https://www.youtube.com.hk/results?" + query_string)
     search_results = re.findall(r'url\"\:\"\/watch\?v\=(.*?(?=\"))', html_content.read().decode())
@@ -15,6 +21,10 @@ def dragyoutube(link):
             return 'fuck you'
 
 def dragbilibili(link):
+    headers = {
+        'origin': 'www.bilibili.com',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
     link=link.replace(' ','+')
     link=quote(link)
     html_content = urllib.request.urlopen("https://search.bilibili.com/all?keyword=" + link)
