f4c90bad06b7de45382bec3a00b78c9eabe19e0a
myc11 <43036493+myc11@users.noreply.github.com>
2022-11-14 05:50:56 +0800

    Changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot('!', intents=intents)
+bot = commands.Bot('$', intents=intents)
 
 extensions = [
     'cogs.music',
