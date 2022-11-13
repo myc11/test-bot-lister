52c7a7e81bfc2876f5791291efd03c6b218a8f49
RainbowPuppeteer <chengzong.zhao@gmail.com>
2022-11-13 16:16:14 -0500

    loop

--- utils/musicyoutube.py
+++ utils/musicyoutube.py
@@ -13,9 +13,6 @@ from utils.download import download_audio_local
 from utils.youtubevideoname import getyoutube_name
 from utils.requrst import dragyoutube
 
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
-}
 
 def download_audio_local(link):
     path=download_audio_local(link)
@@ -27,7 +24,11 @@ def download_audio_local(link):
         return Song(name, source, path)
 
 def download_audio_global(link):
+    ffmpeg_options = {
+        'before_options': f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -headers $\'origin:https://www.youtube.com\r\nreferer:{link}\r\nuser-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36\r\n\''
+    }
     try:
+
         ydl_opts = {'format': 'bestaudio'}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(link, download=False)
--- utils/playlist.py
+++ utils/playlist.py
@@ -1,35 +1,58 @@
+from utils.log import Log
 class Playlist:
 
     def __init__(self):
         self.queue = []
         self.loop_song = False
         self.loop_queue = False
-
+        self.loop_skip = False
         self.playing = None
 
     def __len__(self):
         return len(self.queue)
 
     def __str__(self):
-        return f'Song loop: {self.loop_song}  Queue loop: {self.loop_queue}\n'+'\n'.join([f'{i}. {song.name}'for i, song in enumerate(self.queue)])
+        songname = self.playing.name if self.playing is not None else 'None'
+        if len(self) != 0:
+            return f'Song loop: {self.loop_song}  Queue loop: {self.loop_queue}\n'+f'Playing: \
+                    {songname}\n'+'\n'.join([f'{i + 1}. {song.name}'for i, song in enumerate(self.queue)])
+        return f'Song loop: {self.loop_song}  Queue loop: {self.loop_queue}\n' + \
+               f'Playing: {songname}\n'+'The playlist is empty'
+
+    def loop(self):
+        self.loop_song = not self.loop_song
+        return self.loop_song
+
+    def loopqueue(self):
+        self.loop_queue = not self.loop_queue
+        return self.loop_queue
 
     def add(self, song):
+        Log.log('playlist', f'{song.name} added')
         self.queue.append(song)
 
     def get(self):
-        if self.playing is not None and self.playing not in self.queue:
+        if self.playing is not None and self.playing not in self.queue and not self.loop_song:
             self.playing.destroy()
 
-        self.playing = None
+        skip = True if self.loop_skip else False
+        self.loop_skip = False
 
         if len(self) == 0:
+            self.playing = None
             return None
+        elif self.loop_song and self.loop_queue:
+            if self.playing is None or skip:
+                self.queue.append(self.playing)
+                self.playing = self.queue.pop(0)
+            return self.playing
         elif self.loop_song:
-            self.playing = self.queue[0]
+            if self.playing is None or skip:
+                self.playing = self.queue.pop(0)
             return self.playing
         elif self.loop_queue:
-            self.playing = self.queue.pop(0)
             self.queue.append(self.playing)
+            self.playing = self.queue.pop(0)
             return self.playing
         else:
             self.playing = self.queue.pop(0)
