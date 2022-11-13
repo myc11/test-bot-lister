cf73a18340cfe74c1d8c3fd2077795cbab18e4d6
RainbowPuppeteer <chengzong.zhao@gmail.com>
2022-11-12 22:10:06 -0500

    skip command

--- utils/voicehandler.py
+++ utils/voicehandler.py
@@ -13,6 +13,8 @@ class VoiceHandler:
         self.playlist = Playlist()
         self.txtchannel = ctx.channel
         self.voice_client = ctx.voice_client
+
+        Log.log('VoiceHandler.__init__', "New handler: "+str(ctx.guild))
         if not self.voice_client.is_connected():
             raise
 
@@ -47,17 +49,24 @@ class VoiceHandler:
         # Clean play list
         self.playlist.clear()
 
+    async def skip(self, ctx):
+        if self.voice_client.is_playing():
+            await ctx.send(f'{self.playlist.playing.name} skipped')
+            self.voice_client.stop()
+        else:
+            await ctx.send(f'No song is playing')
+
 
     async def voice_state(self):
         # Check if is connected to voice channel
         if not self.voice_client.is_connected():
+            Log.log('ERROR', 'NOT CONNECTED TO VOICE CHANNEL')
             raise
 
 
     async def play_song(self):
         await self.voice_state()
-        if self.voice_client.is_playing():
-            self.voice_client.stop()
+
 
         song = self.playlist.get()
 
@@ -65,6 +74,8 @@ class VoiceHandler:
             Log.log("play_song", 'Playing '+song.name)
             await self.txtchannel.send(f'Now playing {song.name}')
             self.voice_client.play(song.source, after=lambda err: self.next_song())
+        else:
+            await self.txtchannel.send(f'No more songs to play')
 
     def next_song(self):
         Log.log('next_song', '_')
@@ -72,33 +83,34 @@ class VoiceHandler:
         self.bot.loop.create_task(coro)
 
     async def load_song_youtube(self, ctx, msg):
+        async with self.txtchannel.typing():
+            song = download_audio_global(msg)
 
-        song = download_audio_global(msg)
+            if song is not None:
+                Log.log('', f'load_song_youtube {song.name}')
+                if self.playlist.playing is None:
+                    self.playlist.add(song)
+                    await self.play_song()
+                else:
+                    self.playlist.add(song)
+                    await ctx.send(f'{song.name} added in queue position {len(self.playlist)}')
 
-        if song is not None:
-            Log.log('load_song_youtube', self.playlist.playing + ' ' + song.name)
-            if self.playlist.playing is None:
-                self.playlist.add(song)
-                await self.play_song()
             else:
-                self.playlist.add(song)
-                await ctx.send(f'{song.name} added in queue position {len(self.playlist)}')
-
-        else:
-            Log.log('ERROR: load_song_youtube', msg)
-            await ctx.send(f'ERROR: load_song_youtube {msg}')
+                Log.log('ERROR: load_song_youtube', msg)
+                await ctx.send(f'ERROR: load_song_youtube {msg}')
 
     async def load_song_bilibili(self, ctx, msg):
-        song = download_bili_audio(msg)
-        if song is not None:
-            Log.log(f'load_song_bilibili {song.name}')
-            if self.playlist.playing is None:
-                self.playlist.add(song)
-                await self.play_song()
-            else:
-                self.playlist.add(song)
-                await ctx.send(f'{song.name} added in queue position {len(self.playlist)}')
+        async with self.txtchannel.typing():
+            song = download_bili_audio(msg)
+            if song is not None:
+                Log.log('', f'load_song_bilibili {song.name}')
+                if self.playlist.playing is None:
+                    self.playlist.add(song)
+                    await self.play_song()
+                else:
+                    self.playlist.add(song)
+                    await ctx.send(f'{song.name} added in queue position {len(self.playlist)}')
 
-        else:
-            Log.log('ERROR: load_song_bilibili', msg)
-            await ctx.send(f'ERROR: load_song_bilibili {msg}')
+            else:
+                Log.log('ERROR: load_song_bilibili', msg)
+                await ctx.send(f'ERROR: load_song_bilibili {msg}')
